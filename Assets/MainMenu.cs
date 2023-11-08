cedcb6b4b2bbc3f83077cbec5a32927132ef2f82
Matthew Gerstner <mjgerstner4@gmail.com>
2023-11-07 22:56:09 -0600

    Initial commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void EggMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitToZoo()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
